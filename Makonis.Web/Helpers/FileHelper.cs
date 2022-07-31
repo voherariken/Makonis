6eeec2917bf09050c066f8ddf952ed949db96963
Riken Vohera <Riken.Vohera@ENCORA.COM>
2022-07-31 14:25:14 +0530

    Changed Filepath to wwwroot

--- Makonis.Web/Helpers/FileHelper.cs
+++ Makonis.Web/Helpers/FileHelper.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Makonis.Web.Helpers.IHelpers;
 using Makonis.Web.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
@@ -12,9 +13,9 @@ namespace Makonis.Web.Helpers
     public class FileHelper : IFileHelper
     {
         private string filePath;
-        public FileHelper(IConfiguration configuration)
+        public FileHelper(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
-            this.filePath = configuration.GetSection("FilePath").Value;
+            this.filePath = Path.Combine(hostingEnvironment.WebRootPath, configuration.GetSection("FilePath").Value);
         }
 
         public void WriteFile(List<CustomerViewModel> customers)
@@ -24,8 +25,12 @@ namespace Makonis.Web.Helpers
 
         public async Task<List<CustomerViewModel>> ReadFile()
         {
-            var text = await File.ReadAllTextAsync(this.filePath);
-            return JsonConvert.DeserializeObject<List<CustomerViewModel>>(text);
+            if (File.Exists(this.filePath))
+            {
+                var text = await File.ReadAllTextAsync(this.filePath);
+                return JsonConvert.DeserializeObject<List<CustomerViewModel>>(text);
+            }
+            else return null;
         }
     }
 }
